090f07532b114582d8242e90e4afb88708702def
Ashish Tiwari <ashishtiwari1314q@gmail.com>
2026-08-22 02:58:26 +0530

    first commit

---
+++ config/db.config.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+export const ConnectDB = async()=>{
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("DATABASE CONNECTED 🥰😍");
+  } catch (error) {
+    console.log((error.message))
+    throw new Error("Something Went wrong 😭😤 ", error);
+  }
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import { ConnectDB } from "./config/db.config.js";
+
+
+dotenv.config()
+
+const app = express();
+ConnectDB()
+
+
+app.listen(process.env.PORT, ()=>{
+  console.log(`Server is runing at http://localhost:${process.env.PORT}`);
+})
